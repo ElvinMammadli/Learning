935a866177317f5bdbadb9a2345a7b2d9824c724
mekhti11 <mekhti11@gmail.con>
2021-12-11 18:43:06 +0300

    Questions and Teacher

---
+++ src/main/java/com/example/demo/Model/Question.java
@@ -0,0 +1,45 @@
+package com.example.demo.Model;
+
+
+@Entity
+@Table(name="Question")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    private String[] answers;
+    private String true_answer;
+
+    public Question(long id, String[] answers, String true_answer) {
+        this.id = id;
+        this.answers = answers;
+        this.true_answer = true_answer;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String[] getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(String[] answers) {
+        this.answers = answers;
+    }
+
+    public String getTrue_answer() {
+        return true_answer;
+    }
+
+    public void setTrue_answer(String true_answer) {
+        this.true_answer = true_answer;
+    }
+}
