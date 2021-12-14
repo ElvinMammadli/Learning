eb9c79009fd0fa950675713c8fc95737e5dea36c
Mekhti Gumbatov <mekhti11@gmail.com>
2021-12-14 22:02:16 +0300

    exams are on the way

--- src/main/java/com/example/demo/Repository/IExamRepository.java
+++ src/main/java/com/example/demo/Repository/IExamRepository.java
@@ -1,5 +1,16 @@
 package com.example.demo.Repository;
 
-public interface IExamRepository {
+import com.example.demo.Model.Exam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IExamRepository extends JpaRepository<Exam,Long> {
+
+    public Optional<Exam> findById(Long id);
+
+    public void createExam(Exam exam);
+
+    public Exam getByClassId(long id);
 
 }
